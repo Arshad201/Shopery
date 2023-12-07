b813f29b9f8461d2ad72e8784309a2475c597e04
Mohammad Arshad <107191856+Arshad201@users.noreply.github.com>
2023-12-08 00:43:14 +0530

    change in path of client

--- backend/app.js
+++ backend/app.js
@@ -38,10 +38,10 @@ const __dirname1 = path.resolve();
 
 if(process.env.NODE_ENV === 'production'){
 
-  app.use(express.static(path.join(__dirname1, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname1, '../frontend/dist')));
 
   app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname1, 'frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname1, '../', 'frontend', 'dist', 'index.html'));
   });
 
 }else{
